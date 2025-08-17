b91bd7f3601472d02eef4051dc638861486682a3
m.kiani <mortzeakianitadi@gmail.com>
2025-08-17 08:59:50 +0330

    chanege ip  to url

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 
 func main() {
 	// ===== Config =====
-	minioEndpoint := "37.32.8.181:32446"
+	minioEndpoint := "stagestr.lucidfirm.ir"
 	accessKey := "stage"
 	secretKey := "Lj5UlGa6v6uUDMUk"
 	bucketName := "test.test123"
